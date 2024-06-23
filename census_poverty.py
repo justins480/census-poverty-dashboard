3f71634e3b382fd0de6fae553cf73ebe277471a0
justins480 <48602775+justins480@users.noreply.github.com>
2024-06-22 20:23:33 -0700

    Update host binding for Render deployment

--- census_poverty.py
+++ census_poverty.py
@@ -106,7 +106,8 @@ def update_table(selected_year):
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    port = int(os.environ.get('PORT', 8050))
+    app.run_server(debug=True, host='0.0.0.0', port=port)
 
 
 
